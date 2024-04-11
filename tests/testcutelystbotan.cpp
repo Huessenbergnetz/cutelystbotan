30c50810dc786982eb9ddb1522ba63baf5498293
Buschmann <mf@huessenbergnetz.de>
2024-04-11 10:42:51 +0200

    Add more tests

--- tests/testcutelystbotan.cpp
+++ tests/testcutelystbotan.cpp
@@ -21,11 +21,18 @@ public:
 private slots:
     void initTestCase();
     void testArgon2();
+    void testArgon2Wrong();
+    void testArgon2InvalidType();
     void benchmarkArgon2();
     void testBcrypt();
+    void testBcryptWrong();
+    void testBcryptInvalidWorkFactor();
     void benchmarkBcrypt();
     void testPasshash9();
+    void testPasshash9Wrong();
+    void testPasshash9InvalidWorkFactor();
     void benchmarkPasshash9();
+    void testInvalidHashString();
 
 private:
     QString password;
@@ -53,6 +60,30 @@ void CutelystBotanTest::testArgon2()
     QVERIFY(CredentialBotan::validatePassword(password, argon2Hash));
 }
 
+void CutelystBotanTest::testArgon2Wrong()
+{
+    QVERIFY(!CredentialBotan::validatePassword(u"some attempt"_qs, argon2Hash));
+}
+
+void CutelystBotanTest::testArgon2InvalidType()
+{
+    constexpr size_t p = 1;
+    constexpr size_t m = 262144;
+    constexpr size_t i = 1;
+    constexpr size_t s = 16;
+    constexpr size_t o = 32;
+
+    QVERIFY(CredentialBotan::createArgon2Password(
+                password.toUtf8(), CredentialBotan::Type::Invalid, p, m, i, s, o)
+                .isEmpty());
+    QVERIFY(CredentialBotan::createArgon2Password(
+                password.toUtf8(), CredentialBotan::Type::Bcrypt, p, m, i, s, o)
+                .isEmpty());
+    QVERIFY(CredentialBotan::createArgon2Password(
+                password.toUtf8(), CredentialBotan::Type::Passhash9, p, m, i, s, o)
+                .isEmpty());
+}
+
 void CutelystBotanTest::benchmarkArgon2()
 {
     QBENCHMARK
@@ -64,6 +95,17 @@ void CutelystBotanTest::benchmarkArgon2()
 void CutelystBotanTest::testBcrypt()
 {
     QVERIFY(CredentialBotan::validatePassword(password, bcryptHash));
+}
+
+void CutelystBotanTest::testBcryptWrong()
+{
+    QVERIFY(!CredentialBotan::validatePassword(u"some attempt"_qs, bcryptHash));
+}
+
+void CutelystBotanTest::testBcryptInvalidWorkFactor()
+{
+    QVERIFY(CredentialBotan::createBcryptPassword(password.toUtf8(), 3, 'a').isEmpty());
+    QVERIFY(CredentialBotan::createBcryptPassword(password.toUtf8(), 19, 'a').isEmpty());
 }
 
 void CutelystBotanTest::benchmarkBcrypt()
@@ -79,12 +121,34 @@ void CutelystBotanTest::testPasshash9()
     QVERIFY(CredentialBotan::validatePassword(password, passhash9Hash));
 }
 
+void CutelystBotanTest::testPasshash9Wrong()
+{
+    QVERIFY(!CredentialBotan::validatePassword(u"some attempt"_qs, passhash9Hash));
+}
+
+void CutelystBotanTest::testPasshash9InvalidWorkFactor()
+{
+    QVERIFY(CredentialBotan::createPasshash9Password(
+                password.toUtf8(), 0, CredentialBotan::Passhash9Algo::HmacSha1)
+                .isEmpty());
+    QVERIFY(CredentialBotan::createPasshash9Password(
+                password.toUtf8(), 513, CredentialBotan::Passhash9Algo::HmacSha1)
+                .isEmpty());
+}
+
 void CutelystBotanTest::benchmarkPasshash9()
 {
     QBENCHMARK
     {
         CredentialBotan::validatePassword(password, passhash9Hash);
     }
+}
+
+void CutelystBotanTest::testInvalidHashString()
+{
+    QString invalid = argon2Hash;
+    invalid.replace(1, 1, 'b');
+    QVERIFY(!CredentialBotan::validatePassword(password, invalid));
 }
 
 QTEST_MAIN(CutelystBotanTest)
